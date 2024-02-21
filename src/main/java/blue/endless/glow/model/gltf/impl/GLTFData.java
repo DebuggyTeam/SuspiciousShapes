45fa84f47fea8b5a3169a273cbac2e2e49287d31
Kneelawk <kneelawk@gmail.com>
2024-02-21 03:27:00 -0800

    Flesh out GLTFData from 2.0 spec

--- src/main/java/blue/endless/glow/model/gltf/impl/GLTFData.java
+++ src/main/java/blue/endless/glow/model/gltf/impl/GLTFData.java
@@ -19,6 +19,7 @@ import com.google.gson.JsonObject;
 import blue.endless.glow.model.Vector2d;
 import blue.endless.glow.model.Vector3d;
 
+// Refer to https://registry.khronos.org/glTF/specs/2.0/glTF-2.0.html
 public class GLTFData {
 	//PrimitiveType
 	public static final int GL_POINTS         = 0;
@@ -35,15 +36,22 @@ public class GLTFData {
 	public static final int GL_USHORT = 5123;
 	public static final int GL_INT    = 5125;
 	public static final int GL_FLOAT  = 5126;
+	//Wrap
+	public static final int GL_REPEAT = 10497;
 
-	public GLTFAsset asset = new GLTFAsset();
-	public GLTFBufferView[] bufferViews = new GLTFBufferView[0];
-	public GLTFBuffer[] buffers = new GLTFBuffer[0];
+	public String[] extensionsUsed = new String[0];
+	public String[] extensionsRequired = new String[0];
 	public GLTFAccessor[] accessors = new GLTFAccessor[0];
-	public GLTFMesh[] meshes = new GLTFMesh[0];
-	public GLTFMaterial[] materials = new GLTFMaterial[0];
-	public GLTFTexture[] textures = new GLTFTexture[0];
+	public GLTFAnimation[] animations = new GLTFAnimation[0];
+	public GLTFAsset asset = new GLTFAsset();
+	public GLTFBuffer[] buffers = new GLTFBuffer[0];
+	public GLTFBufferView[] bufferViews = new GLTFBufferView[0];
 	public GLTFImage[] images = new GLTFImage[0];
+	public GLTFMaterial[] materials = new GLTFMaterial[0];
+	public GLTFMesh[] meshes = new GLTFMesh[0];
+	public GLTFNode[] nodes = new GLTFNode[0];
+	public GLTFSampler[] samplers = new GLTFSampler[0];
+	public GLTFTexture[] textures = new GLTFTexture[0];
 	public JsonObject extensions = null;
 	public JsonElement extras = null;
 
@@ -176,12 +184,92 @@ public class GLTFData {
 		}
 	}
 
+	public static class GLTFAccessor {
+		public int bufferView = 0;
+		public int byteOffset = 0;
+		public int componentType = 0;
+		public boolean normalized = false;
+		public int count = 0;
+		public String type = "VOID";
+		public float[] max = new float[16];
+		public float[] min = new float[16];
+		public GLTFAccessorSparse sparse = null;
+		public String name = null;
+
+		public GLTFAccessor() {}
+	}
+
+	public static class GLTFAccessorSparse {
+		public int count = 0;
+		public GLTFAccessorSparseIndices indices = new GLTFAccessorSparseIndices();
+		public GLTFAccessorSparseValues values = new GLTFAccessorSparseValues();
+	}
+
+	public static class GLTFAccessorSparseIndices {
+		public int bufferView = 0;
+		public int byteOffset = 0;
+		public int componentType = 0;
+
+		public GLTFAccessorSparseIndices() {}
+	}
+
+	public static class GLTFAccessorSparseValues {
+		public int bufferView = 0;
+		public int byteOffset = 0;
+
+		public GLTFAccessorSparseValues() {}
+	}
+
+	public static class GLTFAnimation {
+		public GLTFAnimationChannel[] channels = new GLTFAnimationChannel[0];
+		public GLTFAnimationSampler[] samplers = new GLTFAnimationSampler[0];
+		public String name = null;
+
+		public GLTFAnimation() {}
+	}
+
+	public static class GLTFAnimationChannel {
+		public int sampler = 0;
+		public GLTFAnimationChannelTarget target = new GLTFAnimationChannelTarget();
+
+		public GLTFAnimationChannel() {}
+	}
+
+	public static class GLTFAnimationChannelTarget {
+		public int node = -1;
+		public String path = null;
+		public JsonObject extensions = null;
+
+		public GLTFAnimationChannelTarget() {}
+	}
+
+	public static class GLTFAnimationSampler {
+		/**
+		 * Accessor.
+		 */
+		public int input = 0;
+		public String interpolation = "LINEAR";
+		/**
+		 * Accessor.
+		 */
+		public int output = 0;
+
+		public GLTFAnimationSampler() {}
+	}
+
 	public static class GLTFAsset {
 		public String version = "untitiled";
 		public String generator = "unknown";
 		public String copyright = null;
 
 		public GLTFAsset() {}
+	}
+
+	public static class GLTFBuffer {
+		public int byteLength = 0;
+		public String uri = "";
+
+		public GLTFBuffer() {}
 	}
 
 	public static class GLTFBufferView {
@@ -194,27 +282,60 @@ public class GLTFData {
 		public GLTFBufferView() {}
 	}
 
-	public static class GLTFBuffer {
-		public int byteLength = 0;
-		public String uri = "";
+	public static class GLTFMaterial {
+		public String name = null;
+		public JsonObject extensions = null;
+		public JsonElement extras = null;
+		public GLTFPBRMetallicRoughness pbrMetallicRoughness = new GLTFPBRMetallicRoughness();
+		public GLTFMaterialNormalTextureInfo normalTexture = new GLTFMaterialNormalTextureInfo();
+		public GLTFMaterialOcclusionTextureInfo occlusionTexture = new GLTFMaterialOcclusionTextureInfo();
+		public GLTFTextureInfo emissiveTexture = new GLTFTextureInfo();
+		public float[] emissiveFactor = {0,0,0};
+		public String alphaMode = "OPAQUE";
+		public float alphaCutoff = 0.5f;
+		public boolean doubleSided = false;
 
-		public GLTFBuffer() {}
+		public GLTFMaterial() {}
 	}
 
-	public static class GLTFAccessor {
-		public int bufferView = 0;
-		public int componentType = 0;
-		public int count = 0;
-		public float[] max = { 0 };
-		public float[] min = { 0 };
-		public String type = "VOID";
+	public static class GLTFPBRMetallicRoughness {
+		public float[] baseColorFactor = {1,1,1,1};
+		public GLTFTextureInfo baseColorTexture = new GLTFTextureInfo();
+		public float metallicFactor = 1;
+		public float roughnessFactor = 1;
+		public GLTFTextureInfo metallicRoughnessTexture = new GLTFTextureInfo();
 
-		public GLTFAccessor() {}
+		public GLTFPBRMetallicRoughness() {}
+	}
+
+	public static class GLTFTextureInfo {
+		public int index = 0;
+		public int texCoord = 0;
+
+		public GLTFTextureInfo() {}
+	}
+
+	public static class GLTFMaterialNormalTextureInfo {
+		public int index = 0;
+		public int texCoord = 0;
+		public float scale = 1;
+
+		public GLTFMaterialNormalTextureInfo() {}
+	}
+
+	public static class GLTFMaterialOcclusionTextureInfo {
+		public int index = 0;
+		public int texCoord = 0;
+		public float strength = 1;
+
+		public GLTFMaterialOcclusionTextureInfo() {}
 	}
 
 	/** Equivalent to glow Model */
 	public static class GLTFMesh {
 		public GLTFPrimitive[] primitives = new GLTFPrimitive[0];
+		public int[] weights = new int[0];
+		public String name = null;
 
 		public GLTFMesh() {}
 	}
@@ -234,35 +355,51 @@ public class GLTFData {
 		public int POSITION = 0;
 		public int NORMAL = 0;
 		public int TEXCOORD_0 = 0;
+		public int JOINTS_0 = 0;
+		public int WEIGHTS_0 = 0;
 
 		public GLTFPrimitiveAttributes() {}
 	}
 
-	public static class GLTFMaterial {
-		public GLTFPBRMetallicRoughness pbrMetallicRoughness = new GLTFPBRMetallicRoughness();
-		public String alphaMode = "MASK";
-		public float alphaCutoff = 0.5f;
-		public boolean doubleSided = false;
+	public static class GLTFNode {
+		public int[] children;
+		public int skin;
+		public float[] matrix = {1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1};
+		public int mesh;
+		public float[] rotation = {0,0,0,1};
+		public float[] scale = {1,1,1};
+		public float[] translation = {0,0,0};
+		public float[] weights = new float[0];
+		public String name = null;
+		public JsonObject extensions = null;
+
+		public GLTFNode() {}
 	}
 
-	public static class GLTFPBRMetallicRoughness {
-		public float metallicFactor = 0;
-		public float roughnessFactor = 0;
-		public GLTFIndexHolder baseColorTexture = new GLTFIndexHolder();
+	public static class GLTFSampler {
+		public int magFilter = 0;
+		public int minFilter = 0;
+		public int wrapS = GL_REPEAT;
+		public int wrapT = GL_REPEAT;
+		public String name = null;
 
-		public GLTFPBRMetallicRoughness() {}
+		public GLTFSampler() {}
 	}
 
-	public static class GLTFIndexHolder {
-		public int index = 0;
+	public static class GLTFSkin {
+		public int inverseBindMatrices = 0;
+		public int skeleton = 0;
+		public int[] joints = new int[0];
+		public String name = null;
 
-		public GLTFIndexHolder() {}
+		public GLTFSkin() {}
 	}
 
 	public static class GLTFTexture {
 		public int sampler = 0;
 		public int source = 0;
 		public String name = null;
+		public JsonObject extensions = null;
 
 		public GLTFTexture() {}
 	}
@@ -272,5 +409,7 @@ public class GLTFData {
 		public String uri = "#all";
 		public int bufferView = -1;
 		public String name = null;
+
+		public GLTFImage() {}
 	}
 }

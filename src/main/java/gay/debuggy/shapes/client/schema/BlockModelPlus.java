56732750fe4503a11e1cd1753e1282c0a13179f3
Falkreon <falkreon@gmail.com>
2025-01-21 07:24:44 -0600

    Test code, add namespace to loader key

--- src/main/java/gay/debuggy/shapes/client/schema/BlockModelPlus.java
+++ src/main/java/gay/debuggy/shapes/client/schema/BlockModelPlus.java
@@ -2,10 +2,14 @@ package gay.debuggy.shapes.client.schema;
 
 import java.util.HashMap;
 
+import com.google.gson.annotations.SerializedName;
+
 import net.minecraft.client.render.model.json.ModelTransformation;
 
 public class BlockModelPlus {
 	public String parent = null;
+	
+	@SerializedName("suspicious_shapes:loader")
 	public String loader = null;
 	public boolean uvlock = false;
 	public HashMap<String, String> textures = new HashMap<>();
